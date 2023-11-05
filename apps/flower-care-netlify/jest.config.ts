995270d93c01255583b72f4e0f43b0c2b1613f2f
Antonio Ruggiero <ant.ruggiero16@gmail.com>
2023-11-05 13:54:48 +0100

    create raspberry and netlify projects

---
+++ apps/flower-care-netlify/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'flower-care-netlify',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': '@swc/jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/flower-care-netlify',
+};
