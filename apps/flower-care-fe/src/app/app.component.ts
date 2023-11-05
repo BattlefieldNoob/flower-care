8a9cb95237c2de1ed82212dc8f5fdf871a5074d4
Antonio Ruggiero <ant.ruggiero16@gmail.com>
2023-11-05 13:42:07 +0100

    Initial commit

---
+++ apps/flower-care-fe/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'flower-care-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'flower-care-fe';
+}
