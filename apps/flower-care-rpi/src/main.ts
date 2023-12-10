8f780491b1b3dfa13ffe51bd3f2a7c45a4c1d502
Antonio Ruggiero <ant.ruggiero16@gmail.com>
2023-12-10 17:51:56 +0000

    bun configuration

--- apps/flower-care-rpi/src/main.ts
+++ apps/flower-care-rpi/src/main.ts
@@ -1,7 +1,8 @@
-import { Effect, Layer, Schedule, pipe } from "effect"
+import { Effect, Schedule, pipe } from "effect"
 import { FlowerCareModule, FlowerCareModuleLive } from "./modules/flower-care.module";
 import { Do, bind } from "effect/Effect";
 import { MiFloraModuleLive } from "./modules/miflora-ble.module";
+import { MiFloraModuleTest } from "./modules/miflora-test.module";
 
 console.log('Hello World');
 
@@ -32,12 +33,7 @@ const flowerCareMacAddress = 'C4:7C:8D:6C:D5:1D';
                 .pipe(Effect.flatMap(() => Effect.logInfo(`Data: ${JSON.stringify(data)}`)))
         }))
 
-
-    const MainLive = MiFloraModuleLive.pipe(
-        Layer.provide(FlowerCareModuleLive)
-    )
-
-    const runnable = Effect.provide(program, MainLive)
+    const runnable = Effect.provide(Effect.provide(program, FlowerCareModuleLive), MiFloraModuleLive)
 
     const retryableRunnable = Effect.retry(
         Effect.log('Executing try of runnable...')
---
+++ apps/my-bun-app/README.md
@@ -0,0 +1,15 @@
+# apps/my-bun-app
+
+This Application was generated with [@nx-bun/nx](https://github.com/jordan-hall/nx-bun)
+
+## Building
+
+Run `nx build apps/my-bun-app` to build the app.
+
+## Running unit serve
+
+Run `nx serve apps/my-bun-app` to execute the unit tests via [bun test](https://bun.sh/docs/cli/test).
+
+## Running unit tests
+
+Run `nx test apps/my-bun-app` to execute the unit tests via [bun test](https://bun.sh/docs/cli/test).
