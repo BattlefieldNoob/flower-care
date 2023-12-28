1927f2e485af2c42ba3b7db2724a38ab474f74c4
Antonio Ruggiero <ant.ruggiero16@gmail.com>
2023-12-28 15:18:28 +0000

    creates graphql service and config

--- apps/flower-care-fe/src/app/app.component.ts
+++ apps/flower-care-fe/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { AllReadingsBetweenGQL } from './generated/graphql';
 
 @Component({
   selector: 'flower-care-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'flower-care-fe';
+
+  readonly allReadingsBetweenGQL = inject(AllReadingsBetweenGQL);
+
+  ngOnInit(): void {
+    this.allReadingsBetweenGQL
+      .fetch({ min: "2023-11-01", max: "2023-12-01" })
+      .subscribe(console.log);
+  }
 }
--- apps/flower-care-fe/src/app/app.module.ts
+++ apps/flower-care-fe/src/app/app.module.ts
@@ -4,14 +4,33 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { InMemoryCache } from '@apollo/client/core';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    ApolloModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory(httpLink: HttpLink) {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'https://red-tree.eu-central-1.aws.cloud.dgraph.io/graphql',
+          }),
+        };
+      },
+      deps: [HttpLink],
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
